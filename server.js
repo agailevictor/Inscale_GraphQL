94370fc8f0f0900b55b8eac81ae1395761ee9ac8
Agaile Victor <agaile.victor@baesystems.com>
2019-08-16 20:07:44 +0800

    Added Winston logger

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import graphqlHTTP from "express-graphql";
 import OwnerSchema from "./src/graphql/schema";
+import winston from "./config/winston";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(morgan('combined', { stream: winston.stream }));
 app.use('/', graphqlHTTP({
     schema: OwnerSchema,
     graphiql: true
