d3484f99e387f35a82f3c4f7f2ffa18b42c3f754
Agaile Victor <agaile.victor@baesystems.com>
2019-08-17 21:35:28 +0800

    Added logger for every request

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ import graphqlHTTP from "express-graphql";
 import OwnerSchema from "./src/graphql/schema";
 import winston from "./config/winston";
 import morgan from "morgan";
+import bodyParser from "body-parser";
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan('combined', { stream: winston.stream }));
-app.use('/', graphqlHTTP({
-    schema: OwnerSchema,
-    graphiql: true
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', graphqlHTTP(request => {
+    winston.info(`QUERY: "${request.body.query}"`);
+    return {
+        schema: OwnerSchema,
+        graphiql: true
+    }
 }));
 
 app.listen(PORT, () => console.log("Server is Up and running at http://localhost:" + PORT));
